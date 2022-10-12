18a718b6e9ffa23b688536962f2b9882db0cb577
Thanh Trung Ngo <thanhtrung@192.168.2.6>
2022-10-12 23:44:33 +0700

    plus, divide, multiplication

---
+++ baitapuit/nhapmonlaptrinh/b. tinh_tong/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a;
+    double b;
+
+    //inout numbers
+    cout << "put in the first number: ";
+    cin >> a;
+    cout << "put in the second number: ";
+    cin >> b;
+
+    //calculating processes
+
+    cout << "sum: " << a + b << endl;
+    cout << "multiplication: " << a*b << endl;
+    cout << "divide: " << a/b << endl;
+}
