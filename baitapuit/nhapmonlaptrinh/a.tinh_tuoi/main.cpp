6a7433e10c5ce6cb65012fae3b36d712515c33ab
Thanh Trung Ngo <thanhtrung@192.168.2.6>
2022-10-12 21:51:53 +0700

    new folder

---
+++ baitapuit/nhapmonlaptrinh/a.tinh_tuoi/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "put in your birth year: " << endl;
+    cin >> year;
+    cout << "your age is: " << 2022 - year;
+}
